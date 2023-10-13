efbab4e782c87e340e806e2a1cc4ddffb8fc2a99
AlejandroPulgarin <alejandro.pulgarin@pricefx.com>
2023-10-13 17:01:26 -0500

    first commit

---
+++ gestion-clientes/src/test/java/com/ms/negocio/gestionclientes/GestionClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.negocio.gestionclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
