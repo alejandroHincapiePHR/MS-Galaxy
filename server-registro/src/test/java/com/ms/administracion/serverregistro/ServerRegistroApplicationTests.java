13000d667d8fbc1ac05597223c0dae122c1e6f6b
AlejandroPulgarin <alejandro.pulgarin@pricefx.com>
2023-10-16 23:07:01 -0500

    server registry added

---
+++ server-registro/src/test/java/com/ms/administracion/serverregistro/ServerRegistroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.administracion.serverregistro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerRegistroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
